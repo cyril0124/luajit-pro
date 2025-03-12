f6ecb783e894a11b629c86c86e17af2fb0f5d16f
cyril0124 <211998078@qq.com>
2025-03-12 10:38:36 +0800

    feat(lua_optimizer): add @used annotation for preserving local variables

--- src/lua_optimizer.rs
+++ src/lua_optimizer.rs
@@ -1,7 +1,10 @@
 use std::collections::HashMap;
 
 use full_moon::{
-    ast::{Expression, Field, Index, Prefix, Stmt, Suffix, Var},
+    ast::{
+        punctuated::{Pair, Punctuated},
+        Expression, Field, Index, Prefix, Stmt, Suffix, Var,
+    },
     tokenizer::TokenType,
     visitors::VisitorMut,
 };
@@ -24,11 +27,16 @@ impl VisitorMut for LuaOptimizer {
             Stmt::LocalAssignment(local_assignment) => {
                 let local_tk = local_assignment.local_token();
                 let mut has_annotation = false;
+                let mut annotation_name = String::new();
                 local_tk.trailing_trivia().into_iter().for_each(|trivia| {
                     match trivia.token_type() {
                         TokenType::MultiLineComment { blocks, comment } => {
-                            if blocks == &0 && comment.as_str() == "@comp_time_enum" {
+                            if blocks == &0
+                                && (comment.as_str() == "@comp_time_enum"
+                                    || comment.as_str() == "@used")
+                            {
                                 has_annotation = true;
+                                annotation_name = comment.as_str().to_string();
                             }
                         }
                         _ => {}
@@ -45,75 +53,133 @@ impl VisitorMut for LuaOptimizer {
                         })
                         .collect();
                     if name_vec.len() == 1 {
-                        let enum_name = name_vec.first().unwrap();
                         assert!(
                             local_assignment.expressions().pairs().count() == 1,
-                            "[@comp_time_enum] Only support one expression!"
+                            "[{annotation_name}] Only support one expression!"
                         );
-                        let mut key_value_map = HashMap::new();
-                        local_assignment.expressions().pairs().for_each(|e| {
-                            let expr = e.clone().into_value();
-                            match expr {
-                                Expression::TableConstructor(tbl_constructor) => {
-                                    tbl_constructor
-                                        .fields()
-                                        .iter()
-                                        .for_each(|field| match field {
-                                            Field::NameKey {
-                                                key,
-                                                equal: _,
-                                                value,
-                                            } => {
-                                                let v = match value {
-                                                    Expression::Number(number) => {
-                                                        Some(number.token().to_string())
+
+                        match annotation_name.as_str() {
+                            "@comp_time_enum" => {
+                                //
+                                // A compile-time enum optimization which will replace the enum expression with its value
+                                //
+                                // Example:
+                                //      local --[[@comp_time_enum]] enum = {
+                                //          A = 0x123,
+                                //          B = 456,
+                                //      }
+                                //      local var = enum.A
+                                // will be transformed to:
+                                //      local --[[@comp_time_enum]] enum = {
+                                //          A = 0x123,
+                                //          B = 456,
+                                //      }
+                                //      local var = 0x123 --[[enum.A]]
+                                //
+                                let enum_name = name_vec.first().unwrap();
+                                let mut key_value_map = HashMap::new();
+                                local_assignment.expressions().pairs().for_each(|e| {
+                                    let expr = e.clone().into_value();
+                                    match expr {
+                                        Expression::TableConstructor(tbl_constructor) => {
+                                            tbl_constructor
+                                                .fields()
+                                                .iter()
+                                                .for_each(|field| match field {
+                                                    Field::NameKey {
+                                                        key,
+                                                        equal: _,
+                                                        value,
+                                                    } => {
+                                                        let v = match value {
+                                                            Expression::Number(number) => {
+                                                                Some(number.token().to_string())
+                                                            }
+                                                            Expression::String(str) => {
+                                                                Some(str.token().to_string())
+                                                            }
+                                                            _ => {
+                                                                println!("[luajit_pro_helper] [@comp_time_enum] Unexpected Expression: <{}>, enum_name: {}", value.to_string(), enum_name);
+                                                                None
+                                                            }
+                                                        };
+                                                        if v.is_some() {
+                                                            key_value_map.insert(key.token().to_string(), v.unwrap());
+                                                        }
                                                     }
-                                                    Expression::String(str) => {
-                                                        Some(str.token().to_string())
+                                                    Field::ExpressionKey {
+                                                        brackets: _,
+                                                        key: _,
+                                                        equal: _,
+                                                        value: _,
+                                                    } => {
+                                                        todo!("{}", field.to_string())
                                                     }
-                                                    _ => {
-                                                        println!("[luajit_pro_helper] [@comp_time_enum] Unexpected Expression: <{}>, enum_name: {}", value.to_string(), enum_name);
-                                                        None
-                                                    }
-                                                };
-                                                if v.is_some() {
-                                                    key_value_map.insert(key.token().to_string(), v.unwrap());
-                                                }
-                                            }
-                                            Field::ExpressionKey {
-                                                brackets: _,
-                                                key: _,
-                                                equal: _,
-                                                value: _,
-                                            } => {
-                                                todo!("{}", field.to_string())
-                                            }
-                                            _ => {}
-                                        });
+                                                    _ => {}
+                                                });
+                                        }
+                                        _ => panic!("[@comp_time_enum] Unexpected Expression: {:?}", expr),
+                                    }
+                                });
+                                if self.enum_map.is_none() {
+                                    self.enum_map = Some(HashMap::new());
+                                    // println!("enum: {} => <{:#?}>", enum_name, key_value_map);
+                                    self.enum_map
+                                        .as_mut()
+                                        .unwrap()
+                                        .insert(enum_name.clone(), key_value_map);
+                                } else {
+                                    // println!("enum: {} => <{:#?}>", enum_name, key_value_map);
+                                    self.enum_map
+                                        .as_mut()
+                                        .unwrap()
+                                        .insert(enum_name.clone(), key_value_map);
                                 }
-                                _ => panic!("[@comp_time_enum] Unexpected Expression: {:?}", expr),
+
+                                Stmt::LocalAssignment(local_assignment)
                             }
-                        });
-                        if self.enum_map.is_none() {
-                            self.enum_map = Some(HashMap::new());
-                            // println!("enum: {} => <{:#?}>", enum_name, key_value_map);
-                            self.enum_map
-                                .as_mut()
-                                .unwrap()
-                                .insert(enum_name.clone(), key_value_map);
-                        } else {
-                            // println!("enum: {} => <{:#?}>", enum_name, key_value_map);
-                            self.enum_map
-                                .as_mut()
-                                .unwrap()
-                                .insert(enum_name.clone(), key_value_map);
+                            "@used" => {
+                                //
+                                // Mark the local variable as used so that it can not be optimized out.
+                                //
+                                // This is useful for the case where the user wants to use `debug.getlocal`
+                                // to get all the local variables available in the current scope where the
+                                // variable is supposed to be optimized out.
+                                //
+                                // Example:
+                                //      local --[[@used]] abc = 1234
+                                // will be transformed to:
+                                //      local --[[@used]] abc = 1234 _ = abc
+                                // where _ is a dummy variable that is used to mark the variable as used
+                                //
+                                let local_name = name_vec.first().unwrap();
+                                let mut punc_expr: Punctuated<Expression> = Punctuated::new();
+                                local_assignment.expressions().pairs().for_each(|e| {
+                                    let expr = e.clone().into_value();
+                                    punc_expr.push(Pair::new(
+                                        ast_utilis::insert_after_expr(
+                                            &expr,
+                                            format!(" _ = {local_name} ").as_str(),
+                                        ),
+                                        None,
+                                    ));
+
+                                    log::debug!(
+                                        "[luajit-pro] [@used] Inserted dummy variable: {}",
+                                        local_name
+                                    );
+                                });
+
+                                Stmt::LocalAssignment(local_assignment.with_expressions(punc_expr))
+                            }
+                            _ => panic!("Unknown annotation: {}", annotation_name),
                         }
                     } else {
-                        panic!("[@comp_time_enum] Should only have one enum name!");
+                        panic!("[{annotation_name}] Should only have one variable name!");
                     }
+                } else {
+                    node
                 }
-
-                node
             }
             _ => node,
         }
